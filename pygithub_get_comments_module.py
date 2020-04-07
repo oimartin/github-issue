c9770984ed3944e0589ec121666ec8ba77d9785b
Olivia M <olivia.martin.473@gmail.com>
2020-04-07 13:59:39 -0500

    refactor: add docstring to remaining functions, add: Pipfile.loc (dependencies)

--- pygithub_get_comments_module.py
+++ pygithub_get_comments_module.py
@@ -1,9 +1,21 @@
+"""Use PyGithub to connect to Github API v3.
+
+Returns:
+    class -- github connects with Github API v3
+
+"""
 from github import Github
 import re
 import argparse
 
 
 def parse_cmdline():
+    """Command-line interface.
+
+    Returns:
+        --parsed info -- info for API connect
+
+    """
     parser = argparse.ArgumentParser(
         description='comments from gitHub issues')
     parser.add_argument(
@@ -21,15 +33,27 @@ def parse_cmdline():
 
 
 def github_repo(args):
-    connect = Github(args.token)
-    return connect.get_repo(f"{args.organization}/{args.repository}")
+    """Connect to Github repo.
+
+    Returns:
+        --org/repo -- info for API connect
+
+    """
+    try:
+        connect = Github(args.token)
+        return connect.get_repo(f"{args.organization}/{args.repository}")
+    except ValueError:
+        print(f"Could not connect to repository.")
+        print(f"Check spelling of org and repo.")
 
 
 def issue_comments(repo, id):
-    """output issue content
+    """Output issue content.
+
     Arguments:
         repo {object} -- github repository api object
         id {string} -- issue id
+
     """
     issue = repo.get_issue(number=id)
     print(f"This is the first comment of {issue.id}: {issue.body}")
@@ -43,6 +67,13 @@ def issue_comments(repo, id):
 
 
 def main():
+    """Execute other functions in script.
+
+    Executes:
+        parse_cmdline() -- command-line interface
+        github_repo -- connect to org/repo
+        issue_comments -- output issue content
+    """
     args = parse_cmdline()
     repo = github_repo(args)
     issue_comments(repo, args.issue_id)
