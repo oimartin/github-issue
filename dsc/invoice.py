79aeba582dfcba225b9e8a813f3ebfc18c15db83
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-04-15 15:42:40 -0500

    test: add parsing and testing of emails

--- dsc/invoice.py
+++ dsc/invoice.py
@@ -3,18 +3,43 @@ from html.parser import HTMLParser
 
 class InvoiceHTMLParser(HTMLParser):
     def __init__(self):
+        self._nth_td_tag = 0
+        self._nth_br_tag = 0
         self.nth_strong_tag = 0
+        self._shipto_email = ''
+        self._consumer_email = ''
         self.order_id = ''
         HTMLParser.__init__(self)
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'td':
+            self._nth_td_tag += 1
 
     def handle_endtag(self, tag):
         if tag == 'strong':
             self.nth_strong_tag += 1
 
+    def handle_startendtag(self, tag, attrs):
+        if tag == 'br':
+            if self._nth_td_tag >= 2:
+                self._nth_br_tag += 1
+
     def handle_data(self, data):
         if self.nth_strong_tag == 2:
             if not self.order_id:
                 self.order_id = data
+        if self._nth_br_tag == 8:
+            if not self._shipto_email:
+                self._shipto_email = data
+        if self._nth_br_tag == 17:
+            if not self._consumer_email:
+                self._consumer_email = data
 
     def get_order_id(self):
-        return self.order_id.lstrip()
+        return self.order_id.strip()
+
+    def shipping_email(self):
+        return self._shipto_email.strip()
+
+    def consumer_email(self):
+        return self._consumer_email.strip()
--- tests/test_invoice.py
+++ tests/test_invoice.py
@@ -1,20 +1,31 @@
 import unittest
 from dsc.invoice import InvoiceHTMLParser
+import os
+
+
+TEST_FOLDER = os.path.join(os.path.dirname(__file__))
 
 
 class TestInvoiceHTMLParser(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        input = os.path.join(TEST_FOLDER, 'invoice_data.html')
+        with open(input, 'r') as f:
+            cls.html = f.read()
+
+    def setUp(self):
+        self.parser = InvoiceHTMLParser()
+        self.parser.feed(self.html)
+
     def test_order_id(self):
-        html = """
-            <html>
-                <head>
-                    <title>Parse it </title>
-                </head>
-                <body>
-                    <p><strong>Date:</strong> March 14 2017 14:32:30</p>
-                    <p><strong>Order ID:</strong> 890348</p>
-                </body>
-            </html>
-        """
-        parser = InvoiceHTMLParser()
-        parser.feed(html)
-        self.assertEqual(parser.get_order_id(), '890348')
+        self.assertEqual(self.parser.get_order_id(), '890348')
+
+    def test_shipping_email(self):
+        self.assertEqual(
+            self.parser.shipping_email(),
+            'david.knecht@uconn.edu')
+
+    def test_consumer_email(self):
+        self.assertEqual(
+            self.parser.consumer_email(),
+            'abc@abc.edu')
