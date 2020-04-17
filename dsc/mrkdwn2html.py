07aec066f038924341d97e65a645a9bc79412b09
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-04-17 11:38:13 -0500

    refactor: refactor all method arguments

---
+++ dsc/mrkdwn2html.py
@@ -0,0 +1,29 @@
+"""Markdown to html."""
+import mistune
+
+
+def issue_body(repo, id):
+    """Get issue/order of interest.
+
+    Arguments:
+        repo  -- connect to repo
+        id int -- issue/order id
+
+    Returns:
+        issue -- markdown content of issue body
+
+    """
+    return repo.get_issue(number=id).body
+
+
+def mrkdwn_html(body):
+    """Convert issue body to html.
+
+    Arguments:
+        body -- the markdown content of issue body 
+
+    Returns:
+        issue_body_html -- in html format
+
+    """
+    return mistune.html(body)
