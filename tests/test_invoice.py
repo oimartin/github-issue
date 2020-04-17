d10e1fd7d55a2dfc619479dce83fce37f7417527
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-04-17 11:10:28 -0500

    feat: close #14

--- tests/test_invoice.py
+++ tests/test_invoice.py
@@ -1,31 +1,41 @@
-"""Parse html."""
 import unittest
 from dsc.invoice import InvoiceHTMLParser
-import dsc.mrkdwn_html
+import os
+
+
+TEST_FOLDER = os.path.join(os.path.dirname(__file__))
 
 
 class TestInvoiceHTMLParser(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.mrkdwn_html.mrkdwn_html()
+        input = os.path.join(TEST_FOLDER, 'invoice_data.html')
+        with open(input, 'r') as f:
+            cls.html = f.read()
 
     def setUp(self):
-        """Parse html."""
         self.parser = InvoiceHTMLParser()
         self.parser.feed(self.html)
 
     def test_order_id(self):
-        """Unit test."""
         self.assertEqual(self.parser.get_order_id(), '890348')
 
     def test_shipping_email(self):
-        """Unit test."""
         self.assertEqual(
             self.parser.shipping_email(),
             'david.knecht@uconn.edu')
 
     def test_consumer_email(self):
-        """Unit test."""
         self.assertEqual(
             self.parser.consumer_email(),
             'abc@abc.edu')
+
+    def test_all(self):
+        self.assertEqual(self.parser.get_order_id(), '890348')
+        self.assertEqual(
+            self.parser.shipping_email(),
+            'david.knecht@uconn.edu')
+        self.assertEqual(
+            self.parser.consumer_email(),
+            'abc@abc.edu')
+
