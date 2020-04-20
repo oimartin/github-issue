b34dd91f86d58fb9846b9f65fc0ee6238c68e6cb
Olivia M <olivia.martin.473@gmail.com>
2020-04-20 11:57:36 -0500

    feat: get user name of issue body

--- dsc/invoice.py
+++ dsc/invoice.py
@@ -11,6 +11,7 @@ class InvoiceHTMLParser(HTMLParser):
         self._shipto_email = ''
         self._consumer_email = ''
         self.order_id = ''
+        self._user_name = ''
         HTMLParser.__init__(self)
 
     def handle_starttag(self, tag, attrs):
@@ -34,6 +35,9 @@ class InvoiceHTMLParser(HTMLParser):
         if self.nth_strong_tag == 2:
             if not self.order_id:
                 self.order_id = data
+        if self._nth_td_tag == 2:
+            if not self._user_name:
+                self._user_name = data
         if self._nth_br_tag == 8:
             if not self._shipto_email:
                 self._shipto_email = data
@@ -44,6 +48,10 @@ class InvoiceHTMLParser(HTMLParser):
     def get_order_id(self):
         """Retrieve order id."""
         return self.order_id.strip()
+
+    def get_user_name(self):
+        """Retrieve order id."""
+        return self._user_name.strip()
 
     def get_shipping_email(self):
         """Retrieve shipping email."""
