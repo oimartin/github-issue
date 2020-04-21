4311a44031ab31cbd782f12687d39ad24771d96d
Olivia M <olivia.martin.473@gmail.com>
2020-04-21 18:53:04 -0500

    feat: begin making a unit test for sending emails

---
+++ tests/dicty_order_data.py
@@ -0,0 +1,11 @@
+"""Dictionary order data"""
+
+
+def info():
+    order = {'id': '4628',
+             'user_name': 'David Knecht1',
+             'shipping_email': 'oimartin1015@gmail.com',
+             'trigger_label': 'Growing/InPreparation',
+             'mailgun_key': 'e60d8622565dbe68fb1f29bae4f892eb-915161b7-25c6384f',
+             'mailgun_domain': 'sandboxe866881ecbfb41dfb4edf4dc44fbc482.mailgun.org'}
+    return order
