c2440f524fa472a6b728b2495d559042dc6537d8
Olivia M <olivia.martin.473@gmail.com>
2020-04-21 15:08:40 -0500

    fix: add key & domain to dict generated from html

--- dsc/dict.py
+++ dsc/dict.py
@@ -1,9 +1,11 @@
 """Send order info into a dictionary."""
 
 
-def dict_order_info(order_id, user_name, shipping_email, label):
+def dict_order_info(order_id, user_name, shipping_email, label, key, domain):
     order = {'id': order_id,
              'user_name': user_name,
              'shipping_email': shipping_email,
-             'trigger_label': label}
+             'trigger_label': label,
+             'mailgun_key': key,
+             'mailgun_domain': domain}
     return order
