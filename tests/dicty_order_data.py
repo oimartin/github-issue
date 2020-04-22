24d3c1ad0543a7fc85a606cfe0f0599cac22bf93
Olivia M <olivia.martin.473@gmail.com>
2020-04-22 10:41:07 -0500

    chore: update gitignore and data for test email

--- tests/dicty_order_data.py
+++ tests/dicty_order_data.py
@@ -1,4 +1,5 @@
 """Dictionary order data"""
+from config import key
 
 
 def info():
@@ -6,6 +7,6 @@ def info():
              'user_name': 'David Knecht1',
              'shipping_email': 'oimartin1015@gmail.com',
              'trigger_label': 'Growing/InPreparation',
-             'mailgun_key': 'e60d8622565dbe68fb1f29bae4f892eb-915161b7-25c6384f',
+             'mailgun_key': key,
              'mailgun_domain': 'sandboxe866881ecbfb41dfb4edf4dc44fbc482.mailgun.org'}
     return order['id'], order['user_name'], order['shipping_email'], order['trigger_label'], order['mailgun_key'], order['mailgun_domain']
