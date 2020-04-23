b3fee867c3259d464d34baa09be94b33e55557bd
Olivia M <olivia.martin.473@gmail.com>
2020-04-23 14:54:48 -0500

    refactor: lessen parameters being called in main

--- pygithub_get_comments_module.py
+++ pygithub_get_comments_module.py
@@ -1,5 +1,5 @@
 """Use PyGithub to connect to Github API v3."""
-from dsc.cmdline import parse_cmdline
+from dsc.cmdline import parse_cmdline, cmdline_tuple
 from dsc.connect import github_repo
 from dsc.mrkdwn2html import issue_body, mrkdwn_html
 from dsc.invoice import InvoiceHTMLParser
@@ -18,19 +18,15 @@ def main():
         order_info -- order id, shipping and consumer emails
     """
     args = parse_cmdline()
-    repo = github_repo(args.token, args.organization, args.repository)
-    mrkdwn = issue_body(repo, args.issue_id)
+    info = cmdline_tuple(args)
+    repo = github_repo(info)
+    mrkdwn = issue_body(repo, info)
     html = mrkdwn_html(mrkdwn)
     parser = InvoiceHTMLParser()
     parser.feed(html)
-    order = dict_order_info(parser.get_order_id(), parser.get_user_name(),
-                            parser.get_shipping_email(), args.label,
-                            args.apikey, args.domain)
-    email_msg = send_email(order['id'], order['user_name'],
-                           order['shipping_email'],
-                           order['trigger_label'],
-                           order['mailgun_key'],
-                           order['mailgun_domain'])
+    order = dict_order_info(parser.get_all_order_info(),
+                            info)
+    email_msg = send_email(order)
     return email_msg
 
 
