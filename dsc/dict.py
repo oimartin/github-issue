d96b52016a7b5043d1ffc5daeb9476c6271d145e
Olivia M <olivia.martin.473@gmail.com>
2020-04-23 14:50:33 -0500

    refactor: call dict and use command line tuple

--- dsc/dict.py
+++ dsc/dict.py
@@ -1,11 +1,12 @@
 """Send order info into a dictionary."""
 
 
-def dict_order_info(order_id, user_name, shipping_email, label, key, domain):
-    order = {'id': order_id,
-             'user_name': user_name,
-             'shipping_email': shipping_email,
-             'trigger_label': label,
-             'mailgun_key': key,
-             'mailgun_domain': domain}
+def dict_order_info(order, issue):
+    order = {'id': order[0],
+             'user_name': order[1],
+             'shipping_email': order[2],
+             'trigger_label': issue[3],
+             'mailgun_key': issue[5],
+             'mailgun_apicall': issue[4],
+             'mailgun_domain': issue[7]}
     return order
