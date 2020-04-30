62d603e6abf90341e7e3869381c98d4a70b73801
Olivia M <olivia.martin.473@gmail.com>
2020-04-30 10:25:34 -0500

    refactor: new class EmailUser

--- dsc/email.py
+++ dsc/email.py
@@ -1,19 +1,32 @@
-
-"""Send user update email."""
 import requests
 
 
-def send_email(order):
-    """Create email template to update users."""
-    email = {"from": "Excited User <postmaster@" + order['mailgun_domain'],
-             "to": order['shipping_email'],
-             "subject": f"DSC Order {order['id']} - {order['trigger_label']}",
-             "text": f"""Dear {order['user_name']},
-              Your order status: {order['trigger_label']}
-              Please let us know if you have any questions.
-              Best regards,
-              The DSC Team
-              dictystocks@northwestern.edu"""}
+class EmailUser:
+    """Use Mailgun API to send user order update email."""
 
-    return requests.post(order['mailgun_apicall'],
-                         auth=("api", order['mailgun_key']), data=email)
+    def __init__(self, order):
+        """Initilize order information attributes."""
+        self.sender = f"Excited User <postmaster@{order['mailgun_domain']}"
+        self.recipient = order['shippiing_email']
+        self.subject = f"DSC Order {order['id']} - {order['trigger_label']}"
+        self.text = f"""Dear {order['user_name']},
+                Your order status: {order['trigger_label']}
+                Please let us know if you have any questions.
+                Best regards,
+                The DSC Team
+                dictystocks@northwestern.edu"""
+        self.mailgun_apicall = order.mailgun_apicall
+        self.mailgun_key = order.mailgun_key
+
+    def create_email(self):
+        """Create email template to update users."""
+        email = {"from": self.sender,
+                 "to": self.recipient,
+                 "subject": self.subject,
+                 "text": self.text}
+        return email
+
+    def send_email(self):
+        """Initialize email with mailgun API."""
+        return requests.post(self.mailgun_apicall,
+                             auth=("api", self.mailgun_key), data=self.email)
