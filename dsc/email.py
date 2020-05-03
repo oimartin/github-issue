eb0e0aa32f3675e3e7328774be57b40887cd62ab
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-05-03 11:19:27 -0500

    feat:add EmailTemplate class using dataclasses

--- dsc/email.py
+++ dsc/email.py
@@ -1,5 +1,31 @@
 import requests
 from dsc.params import MailgunEmailParams, SendEmailParams
+from dataclasses import dataclass
+from string import Template
+
+
+@dataclass
+class EmailTemplate:
+    subject_template: Template = Template('DSC Order $issue_id - $label')
+    content_template: Template = Template(
+                                """Dear $user,
+                                Your order status: $label
+                                Please let us know if you have any questions.
+                                Best regards,
+                                The DSC Team
+                                dictystocks@northwestern.edu""")
+
+    def generate_subject(self, issue_id: int, label: str) -> str:
+        return self.subject_template.substitute(
+            issue_id=issue_id,
+            label=label
+        )
+
+    def generate_content(self, user: str, label: str) -> str:
+        return self.content_template.substitute(
+            user=user,
+            label=label
+        )
 
 
 class Email:
@@ -20,18 +46,3 @@ class Email:
                   'subject': params.subject,
                   'text': params.content
                   })
-                  
-                "from":
-                f"Excited User <postmaster@{order.mailgun_domain}",
-                "to": order.shipping_email,
-                "subject": f"DSC Order {order.issue_id} - {order.trigger_label}",
-                "text":
-
-
-
-                            """Dear {order.user_name},
-                              Your order status: {order.trigger_label}
-                              Please let us know if you have any questions.
-                              Best regards,
-                              The DSC Team
-                              dictystocks@northwestern.edu"""}
